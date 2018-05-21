1a8723997c921383e3d954448608bdad5be63804
JacobMarx <39311286+JacobMarx@users.noreply.github.com>
2018-05-21 10:49:27 -0700

    Hopefully Works

--- src/PlayerOne.java
+++ src/PlayerOne.java
@@ -1,52 +1,51 @@
-import java.awt.Color;
 
 public class PlayerOne extends CPieces {
 
-	int[] piecesrows = new int[12];
-	int[] piecescolumns = new int[12];
+	public static int[] piecesrowsone = new int[12];
+	public static int[] piecescolumnsone = new int[12];
 	
 	public void PlayerOne() {
 		
-		for (int i = 0; i < piecesrows.length -1; i ++) {
+		for (int i = 0; i < piecesrowsone.length; i ++) {
 			if (i >= 0 && i <= 3) {
-				piecesrows[i] = 0;
+				piecesrowsone[i] = 0;
 			}
 			if (i >= 5 && i <= 7) {
-				piecesrows[i] = 1;
+				piecesrowsone[i] = 1;
 			}
 			if (i >= 8 && i <= 11) {
-				piecesrows[i] = 2;
+				piecesrowsone[i] = 2;
 			}
 		
 		}
-		for (int j = 0; j < piecescolumns.length -1; j++) {
+		for (int j = 0; j < piecescolumnsone.length; j++) {
 			if (j >= 0 && j <= 3) {
-				piecescolumns[j] = 2 * j + 1;
+				piecescolumnsone[j] = 2 * j + 1;
 			}
 			if (j >= 4 && j <= 7) {
-				piecescolumns[j] = 2 * j - 10;
+				piecescolumnsone[j] = 2 * j - 10;
 			}
 			if (j >= 8 && j <= 11) {
-				piecescolumns[j] = 2 * j - 17;
+				piecescolumnsone[j] = 2 * j - 17;
 			}
 		}
 		
-		for (int i = 0; i < piecesrows.length -1; i++) {
-			piecesrows[i] = x;
+		for (int i = 0; i < piecesrowsone.length; i++) {
+			piecesrowsone[i] = x;
 		}
 		
-		for (int i = 0; i < piecescolumns.length -1; i++) {
-			piecescolumns[i] = y;
+		for (int i = 0; i < piecescolumnsone.length; i++) {
+			piecescolumnsone[i] = y;
 		}
 		
 	}
 	
-	int getPOnex(int row) {
-		return piecesrows[row];
+	public static int getPOnex(int row) {
+		return piecesrowsone[row];
 	}
 	
-	int getPOney(int column) {
-		return piecescolumns[column];
+	public static int getPOney(int column) {
+		return piecescolumnsone[column];
 	}
 	
 }
